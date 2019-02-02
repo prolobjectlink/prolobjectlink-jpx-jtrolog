27ee8384a694fecdccf2e53a52abd0fbdaf6fc61
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-02-02 13:50:59 -0400

    Replace WorkLogic by Prolobjectlink

---
+++ src/main/java/org/prolobjectlink/prolog/jtrolog/JTrologHierarchicalCache.java
@@ -0,0 +1,41 @@
+/*
+ * #%L
+ * prolobjectlink-jpp-jtrolog
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.jtrolog;
+
+import org.prolobjectlink.db.ContainerFactory;
+import org.prolobjectlink.db.HierarchicalCache;
+import org.prolobjectlink.db.ObjectConverter;
+import org.prolobjectlink.db.etc.Settings;
+import org.prolobjectlink.db.prolog.PrologHierarchicalCache;
+import org.prolobjectlink.prolog.PrologProvider;
+import org.prolobjectlink.prolog.PrologTerm;
+
+public class JTrologHierarchicalCache extends PrologHierarchicalCache implements HierarchicalCache {
+
+	public JTrologHierarchicalCache(PrologProvider provider, Settings settings, ContainerFactory containerFactory) {
+		super(provider, settings, new JTrologContainerFactory(settings));
+	}
+
+	public JTrologHierarchicalCache(PrologProvider provider, Settings settings, ObjectConverter<PrologTerm> converter,
+			ContainerFactory containerFactory) {
+		super(provider, settings, converter, new JTrologContainerFactory(settings));
+	}
+
+}
---
+++ src/main/java/org/prolobjectlink/prolog/jtrolog/JTrologPersistenceProvider.java
@@ -0,0 +1,98 @@
+/*
+ * #%L
+ * prolobjectlink-jpp-jtrolog
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.jtrolog;
+
+import static org.prolobjectlink.db.XmlParser.XML;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import javax.persistence.spi.PersistenceProvider;
+import javax.persistence.spi.PersistenceUnitInfo;
+
+import org.prolobjectlink.db.DatabaseEngine;
+import org.prolobjectlink.db.DatabaseSchema;
+import org.prolobjectlink.db.DatabaseUser;
+import org.prolobjectlink.db.Protocol;
+import org.prolobjectlink.db.Schema;
+import org.prolobjectlink.db.etc.Settings;
+import org.prolobjectlink.db.jpa.JpaAbstractProvider;
+import org.prolobjectlink.db.jpa.JpaEntityManagerFactory;
+import org.prolobjectlink.db.jpa.JpaProperties;
+import org.prolobjectlink.db.jpa.spi.JPAPersistenceXmlParser;
+import org.prolobjectlink.db.memory.MemoryHierarchical;
+import org.prolobjectlink.db.persistent.EmbeddedHierarchical;
+import org.prolobjectlink.db.persistent.RemoteHierarchical;
+import org.prolobjectlink.db.util.JavaReflect;
+import org.prolobjectlink.logging.LoggerConstants;
+import org.prolobjectlink.logging.LoggerUtils;
+
+public class JTrologPersistenceProvider extends JpaAbstractProvider implements PersistenceProvider {
+
+	public JTrologPersistenceProvider() {
+		JPAPersistenceXmlParser p = new JPAPersistenceXmlParser();
+		URL persistenceXml = Thread.currentThread().getContextClassLoader().getResource(XML);
+		Map<String, PersistenceUnitInfo> m = p.parsePersistenceXml(persistenceXml);
+		for (Entry<String, PersistenceUnitInfo> info : m.entrySet()) {
+			PersistenceUnitInfo unit = info.getValue();
+			String name = unit.getPersistenceUnitName();
+			Properties properties = unit.getProperties();
+			Settings settings = new Settings(properties.getProperty(JpaProperties.DRIVER));
+			URL url = null;
+			try {
+				System.setProperty("java.protocol.handler.pkgs", Protocol.class.getPackage().getName());
+				url = new URL(properties.getProperty(JpaProperties.URL).replace(URL_PREFIX, ""));
+				if (!url.getPath().substring(url.getPath().lastIndexOf('/') + 1).equals(name)) {
+					throw new MalformedURLException("The URL path don't have database named " + name);
+				}
+			} catch (MalformedURLException e) {
+				LoggerUtils.error(MemoryHierarchical.class, LoggerConstants.URL, e);
+			}
+
+			assert url != null;
+
+			// catch the rest of properties
+			String password = properties.getProperty(JpaProperties.PASSWORD);
+			String user = properties.getProperty(JpaProperties.USER);
+			DatabaseUser owner = new DatabaseUser(user, password);
+			Schema schema = new DatabaseSchema(url.getPath(), settings.getProvider(), settings, owner);
+			for (String managedClass : unit.getManagedClassNames()) {
+				schema.addClass(JavaReflect.classForName(managedClass), "");
+			}
+
+			DatabaseEngine e = null;
+			String protocol = url.getProtocol();
+			if (protocol.equalsIgnoreCase(Protocol.MEMPDB.toString())) {
+				e = MemoryHierarchical.newInstance(unit, properties);
+			} else if (protocol.equalsIgnoreCase(Protocol.REMPDB.toString())) {
+				e = RemoteHierarchical.newInstance(unit, properties);
+			} else if (protocol.equalsIgnoreCase(Protocol.FILE.toString())) {
+				e = EmbeddedHierarchical.newInstance(unit, properties);
+			}
+
+			entityManagerFactories.put(name, new JpaEntityManagerFactory(e, properties));
+
+		}
+	}
+
+}
