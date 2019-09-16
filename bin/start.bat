d6cccc92fa4f5e034f0fe08b3b2a731626f2d9d9
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-09-16 06:03:57 -0400

    Update contributing

--- bin/start.bat
+++ bin/start.bat
@@ -5,4 +5,5 @@ for %%B in (%CURRENT_DIRECTORY%.) do set PROLOBJECTLINK_HOME=%%~dpB
 SET CLASSPATH=%PROLOBJECTLINK_HOME%lib\*
 
 : default jdk
+java -classpath %CLASSPATH% org.prolobjectlink.db.prolog.jtrolog.JTrologDatabaseConsole -m
 java -classpath %CLASSPATH% org.prolobjectlink.db.prolog.jtrolog.JTrologDatabaseConsole -z 9110
--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
+java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jtrolog.JTrologDatabaseConsole -m
 java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jtrolog.JTrologDatabaseConsole -z 9110
